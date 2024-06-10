310d6759ef4216da01bca9555af172112606f5ac
victoriaav20 <117371464+victoriaav20@users.noreply.github.com>
2024-06-10 23:44:06 +0200

    interface

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request, jsonify
 import cv2
+import os
+import time
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model
 
 app = Flask(__name__)
 cap = cv2.VideoCapture(0)
@@ -14,28 +20,57 @@ hands = mp_hands.Hands(
     min_tracking_confidence=0.5
 )
 mp_drawing = mp.solutions.drawing_utils
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret:
-        break
+
+# Charger le modèle de reconnaissance des signes
+model = load_model('model_sign_language.h5')
+
+current_prediction = ""  # Variable pour stocker la prédiction courante
+
+def prediction(filename):
+    global current_prediction
     
-    # Convertir l'image de BGR à RGB
-    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    model = load_model('model_sign_language.h5')
+
+    # Charger et prétraiter l'image à comparer
+    img_path = filename
+    img = image.load_img(img_path, target_size=(200, 200))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = img_array / 255.0  # normaliser les pixels entre 0 et 1
+
+    # Faire la prédiction
+    prediction = model.predict(img_array)
+
+    # Afficher la prédiction
+    print(prediction)
+
+    predicted_class_index = np.argmax(prediction)
+    classes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N' ,'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+    predicted_class = classes[predicted_class_index]
+    current_prediction = predicted_class  # Mettre à jour la variable globale
+    print("La classe prédite de l'img", filename, " est :", predicted_class)
+
+def extract_hand_roi(image, landmarks):
+    # Obtenir les coordonnées de la main
+    h, w, _ = image.shape
+    landmark_coords = [(int(landmark.x * w), int(landmark.y * h)) for landmark in landmarks.landmark]
     
-    # Processer l'image et détecter les mains
-    results = hands.process(rgb_frame)
+    # Déterminer la bounding box de la main
+    x_coords, y_coords = zip(*landmark_coords)
+    x_min, x_max = min(x_coords), max(x_coords)
+    y_min, y_max = min(y_coords), max(y_coords)
     
-    # Dessiner les annotations des mains
-    if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
-            mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+    # Ajouter une marge autour de la main pour capturer toute la main
+    margin = 60
+    x_min = max(0, x_min - margin)
+    x_max = min(w, x_max + margin)
+    y_min = max(0, y_min - margin)
+    y_max = min(h, y_max + margin)
     
-    # Afficher l'image annotée
-    cv2.imshow('Hand Tracking', frame)
-    
-    # Sortir de la boucle quand la touche 'q' est pressée
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    # Extraire la ROI de la main
+    hand_roi = image[y_min:y_max, x_min:x_max]
+    return hand_roi
 
 def generate_frames():
     while True:
@@ -44,23 +79,51 @@ def generate_frames():
         if not ret:
             break
         else:
+            # Convertir l'image en RGB pour MediaPipe
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            # Détecter les mains sur l'image
+            results = hands.process(frame_rgb)
+            
+            # Vérifier s'il y a des mains détectées
+            if results.multi_hand_landmarks:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    # Imprimer un message dans la console
+                    print("Main détectée par MediaPipe!")
+                    # Dessiner les annotations des mains détectées sur l'image
+                    mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+                    
+                    # Extraire la ROI de la main
+                    hand_roi = extract_hand_roi(frame, hand_landmarks)
+                    
+                    # Génère un nom de fichier unique
+                    timestamp = int(time.time())
+                    filename = f"captured_frame_{timestamp}.jpg"
+                    
+                    # Sauvegarde la ROI de la main dans un fichier JPEG
+                    cv2.imwrite(filename, hand_roi)
+                    
+                    # Effectuer la comparaison avec le modèle
+                    hand_roi_rgb = cv2.cvtColor(hand_roi, cv2.COLOR_BGR2RGB)
+                    prediction(filename)
+            
+            # Encoder la frame en format JPEG
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-
-
-
 @app.route('/')
 def index():
-    # Affiche la page HTML avec la vidéo en direct
     return render_template('index.html')
 
 @app.route('/video_feed')
 def video_feed():
-    # Retourne la réponse avec les frames de la caméra
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/get_prediction')
+def get_prediction():
+    global current_prediction
+    return jsonify(prediction=current_prediction)
 
 if __name__ == "__main__":
     app.run(debug=True)
